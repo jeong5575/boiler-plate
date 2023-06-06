79dcb8e4fa2f4f55bf2be48524b9fd661e5f2d70
jeong5575 <jeong5575@naver.com>
2023-06-06 17:02:41 +0900

    라우팅 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import Footer from './components/views/Footer/Footer';
+import NavBar from './components/views/NavBar/NavBar';
+import ResgisterPage from './components/views/RegisterPage/ResgisterPage';
+import LandingPage from './components/views/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+    <Routes>
+      <Route path="/" exact element={<LandingPage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<ResgisterPage/>} />
+    </Routes>
     </div>
+  </Router>
   );
 }
 
