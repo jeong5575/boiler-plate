9ffa7c2948c5b1e02c55a57192e5aef1d0cbf9f2
your name <you@example.com>
2023-06-18 14:49:01 +0900

    fix

--- frontend/src/components/views/LandingPage/LandingPage.js
+++ frontend/src/components/views/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ export default function LandingPage() {
   
  const navigate = useNavigate();
 
-const onClickHandler = ()=>{
+const onClickHandlerLogout = ()=>{
   axios.get('/api/users/logout').then(res=>{if(res.data.success)
   
   {navigate('/login') } else alert("로그아웃에 실패하였습니다.")
@@ -14,11 +14,29 @@ const onClickHandler = ()=>{
 
 }
 
+
+const onClickHandlerLogin = ()=>{
+  axios.get('/api/users/login').then(res=>{if(res.data.success)
+  
+  {navigate('/') } else alert("로그인에 실패하였습니다.")
+  })
+
+}
+
+
+
+const onClickHandlerRegister = ()=>{
+  axios.get('/api/users/register').then(res=>{if(res.data.success)
+  
+  {navigate('/register') } else alert("로그인에 실패하였습니다.")
+  })
+
+
   return (
     <div style={{display :'flex',justifyContent:'center',alignItems:'center' ,width:'100%',height:'100vh'}}> 됫다 ㅋㅋ
-    <button onClick={onClickHandler}>로그아웃
-    </button>
-    
+    <button onClick={onClickHandlerLogin}>로그인</button>
+    <button onClick={onClickHandlerLogout}>로그아웃</button>
+    <button onClick={onClickHandlerRegister}>회원가입</button>
     </div>
   )
 }
